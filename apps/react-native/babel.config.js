804bff58ab85d2f816a559cf040b3448dfa85dd0
Coding.Zhu <coding.zhu@italki.com>
2023-10-26 17:34:38 +0800

    fix: rn tailwind conf

--- apps/react-native/babel.config.js
+++ apps/react-native/babel.config.js
@@ -1,9 +1,8 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ["module:metro-react-native-babel-preset"],
+    presets: ['module:metro-react-native-babel-preset'],
     plugins: [
-      // 'react-native-reanimated/plugin',
       'nativewind/babel',
     ],
   }
--- apps/react-native/src/main.tsx
+++ apps/react-native/src/main.tsx
@@ -1,4 +1,6 @@
 import { AppRegistry } from 'react-native';
+// import { NativeWindStyleSheet } from 'nativewind';
 import App from './app/App';
 
+// NativeWindStyleSheet.setOutput({ default: 'native' });
 AppRegistry.registerComponent('ReactNative', () => App);
